0b29be15c609046da048231507472363ae22164a
abikirkham <abbeykirkham9@icloud.com>
2024-04-29 13:15:50 +0200

    update letters guessed to document

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -56,3 +56,20 @@ function guessLetter() {
     // Add the letter to the guessed letters array 
     guessedlist.push(letter);
 }
+ // Update the word display based on the guessed letters 
+    let updatedDisplay = ""; 
+    let allLettersGuessed = true; 
+    for (let i = 0; i < selectedWord.length; i++) { 
+        if (guessedlist.includes(selectedWord[i])) { 
+            updatedDisplay += selectedWord[i] + " "; 
+        } else { 
+            updatedDisplay += "_ "; 
+            allLettersGuessed = false; 
+        } 
+    } 
+    document.getElementById("displayWord").textContent = updatedDisplay; 
+
+    // Check if all letters have been guessed 
+    if (allLettersGuessed) { 
+        alert("Congratulations! You guessed the word correctly!"); 
+    } 
